80dc50d085fedda17892cc35e5474c0c5d97e9be
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-10-11 21:32:28 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
+st.title('Titulo')
+
+data = pd.read_csv('https://raw.githubusercontent.com/labeconometria/MLxE/main/proyectos2do/datasets3.csv')
+pot = data['Potability'].value_counts()
+pot.index = ['NO', 'SI']
+fig, ax = plt.subplots(1,1)
+ax.set_title('Proporción de potabilidad')
+ax.bar(pot.index, pot.values, color = 'orange')
+
+st.pyplot(fig)
+
